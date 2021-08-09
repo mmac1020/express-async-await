5c8811873e74fc1519fb608a06a8d63f2306bf9c
Michael McInerney <michael.mcinerney@fullstackacademy.com>
2021-08-09 12:11:03 -0400

    Adding code from express 201 lecture

--- express/app.js
+++ express/app.js
@@ -4,8 +4,16 @@ const path = require('path');
 
 const app = express();
 
+// __dirname is current directory of app running
+// path.join combines that directory with whatever other arguments
+// mydirectory/public
+// express static takes a string representing the path to the directory of static files
 const expressStaticFiles = express.static(path.join(__dirname, 'public'));
 
+// This now sets up all of our get /fileName routes
+// get /oranges.html
+// get /puppy-1.jpg
+// get /puppy-2.jpg
 app.use(expressStaticFiles);
 
 app.listen(1384, function () {
