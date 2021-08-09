892e220c9ee50bab1102553c8fd413d86c85cca1
Michael McInerney <michael.mcinerney@fullstackacademy.com>
2021-08-09 17:16:20 -0400

    Adding async await code

--- async-await/async-await/simple-async-await.js
+++ async-await/async-await/simple-async-await.js
@@ -1,57 +1,59 @@
 let havePattyInFridge = true;
 
-let getPatty = function() {
-  return new Promise(function(success, failure) {
-    setTimeout(function() {
+let getPatty = function () {
+  return new Promise(function (success, failure) {
+    setTimeout(function () {
       if (havePattyInFridge) {
         success(havePattyInFridge);
       } else {
         failure('Failed to get patty');
-      }  
+      }
     }, 3000);
   });
-}
+};
 
-let cookPatty = function(patty) {
+let cookPatty = function (patty) {
   // we need time to cook the patty!
-  return new Promise(function(success, failure) {
-    setTimeout(function() {
+  return new Promise(function (success, failure) {
+    setTimeout(function () {
       if (patty) {
-        success('We\'ve cooked the patty!');
+        success("We've cooked the patty!");
       } else {
         failure('Failed to cook patty');
       }
-    }, 1000)
-  })
-}
+    }, 1000);
+  });
+};
 
 let haveBunsInStorage = false;
 
-let getBuns = function() {
-  return new Promise(function(success, failure) {
-    setTimeout(function() {
+let getBuns = function () {
+  return new Promise(function (success, failure) {
+    setTimeout(function () {
       if (haveBunsInStorage) {
         success('We got buns!');
       } else {
-        failure('There are no buns')
+        failure('There are no buns');
       }
-    }, 1000)
-  })
-}
+    }, 1000);
+  });
+};
 
-let orderBurger = async function() {
+let orderBurger = async function () {
   try {
     let [patty, buns] = await Promise.all([getPatty(), getBuns()]);
+    // let patty = await getPatty();
+    // let buns = await getBuns();
     console.log(`Got ${patty} and ${buns}`);
     let cookedPatty = await cookPatty(patty);
     serve(cookedPatty);
   } catch (error) {
     console.log(error);
   }
-}
+};
 
-let serve = function(cookedBurger) {
+let serve = function (cookedBurger) {
   console.log(`Order up! ${cookedBurger}`);
-}
+};
 
 orderBurger();
--- async-await/promises/burger-promise.js
+++ async-await/promises/burger-promise.js
@@ -1,22 +1,24 @@
-let canMakeBurger = true;
+let canMakeBurger = false;
 
-let orderBurger = new Promise(
-  function(success, failure) {
-    if (canMakeBurger) {
-      success('We made a burger!');
-    }
-    else {
-      failure('We cannot make a burger');
-    }
+// Library creators will be writing this code.
+let orderBurger = new Promise(function (success, failure) {
+  // If the async work finishes without an error, I MUST call the success callback
+  if (canMakeBurger) {
+    success('We made a burger!');
   }
-)
+  // If the async work finishes WITH an error, I MUST call the failure callback
+  else {
+    failure('We cannot make a burger');
+  }
+});
 
+// WHEN I SEE THIS, I KNOW IT'S A PROMISE!!!!!!
 orderBurger
-  .then(function(successValue) {
+  .then(function (successValue) {
     console.log('Success!');
     console.log(successValue);
   })
-  .catch(function(failureValue) {
+  .catch(function (failureValue) {
     console.log('Failure!');
     console.log(failureValue);
-  })
+  });
