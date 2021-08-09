d6f953182c18c89ac0ac8c7d500f9c6ff88d105b
Michael McInerney <michael.mcinerney@fullstackacademy.com>
2021-08-09 14:47:24 -0400

    Adding insomnia/postman code

--- empty-express/index.js
+++ empty-express/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 
 const app = express();
-
+const morgan = require('morgan');
 // First parameter is request object
 // Second parameter is response object
 // Third we'll get to later
+
+app.use(morgan('dev'));
+// express.json is needed for express to decode a put / post JSON body
+app.use(express.json());
+// app.use((req, res, next) => {
+//   console.log(req.method, req.url);
+
+//   // These will all respond to the client and the request will be responded to. Express stops looking for the route
+//   // res.send
+//   // res.sendFile
+//   // res.json
+
+//   // I don't have a response to send, so continue looking express
+//   next();
+// });
+
 app.get('/', (request, response, next) => {
   // console.log(response);
   response.send('<h1>Hello, welcome to the homepage</h1>');
@@ -16,4 +32,17 @@ app.get('/puppies', (req, res, next) => {
   res.send('<h1>Welcome to the puppies route</h1>');
 });
 
-app.listen(3000);
+app.get('/puppies/:puppyName', (req, res, next) => {
+  console.log(req.params);
+  res.send(`<h1> Hello puppy name: ${req.params.puppyName}</h1>`);
+});
+
+app.put('/puppies/:id', (req, res, next) => {
+  console.log(req.params);
+  console.log(req.body);
+  res.send('WE did it');
+});
+
+app.listen(3000, () => {
+  console.log('The server is live!');
+});
